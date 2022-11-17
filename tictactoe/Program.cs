f194ad0ac9e74bbe245353ffc74a45e860f47229
Orange4me <brandonalder@byui.edu>
2022-11-17 11:00:42 -0700

    idk

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -7,12 +7,12 @@ namespace tictactoe
         static void Main (string[] arg)
         {
             while game is False
-
+            {
                 string board = "1|2|3 \n-+-+- \n4|5|6 \n-+-+- \n7|8|9";
 
                 Console.WriteLine(board);
 
-
+            }
         }
     }
 }
